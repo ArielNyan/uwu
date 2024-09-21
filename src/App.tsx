7881a4581673f92019c13a5e9a119ad5fe03f01c
ArielNyan <arthur.rigotti@gmail.com>
2024-09-21 18:14:39 -0300

    final ig2

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,16 @@ import './App.css'
 import Card from './components/Card'
 function App() {
   return (
-    <div className='h-screen flex justify-center content-center'>
-      <Card />
-    </div>
+    <>
+      <div className='h-screen flex justify-center content-center'>
+        <Card />
+      </div>
+      <footer className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/ArielNyan">ArielNyan</a>.
+      </footer>
+    </>
+
   )
 }
 
